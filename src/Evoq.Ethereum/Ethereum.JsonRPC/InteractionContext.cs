f045406abe9f4dfeed1f59b8e643e4f2ebcd635e
Luke Puplett <lukepuplett@hotmail.com>
2025-04-03 16:39:21 +0100

    Receipt waiting props on InteractionContext

--- src/Evoq.Ethereum/Ethereum.JsonRPC/InteractionContext.cs
+++ src/Evoq.Ethereum/Ethereum.JsonRPC/InteractionContext.cs
@@ -25,4 +25,22 @@ public class InteractionContext : JsonRpcContext<ITransactionFeeEstimate>
             this.FeeEstimateToGasOptions = feeEstimateToGasOptions;
         }
     }
+
+    //
+
+    /// <summary>
+    /// Gets or sets the maximum time to wait for a transaction receipt.
+    /// </summary>
+    public TimeSpan WaitForReceiptTimeout { get; set; } = TimeSpan.FromMinutes(2);
+
+    /// <summary>
+    /// Gets or sets whether to wait for the transaction receipt.
+    /// When set to false, WaitForReceiptTimeout is set to zero.
+    /// </summary>
+    public bool ShouldWaitForReceipt
+    {
+        get => this.WaitForReceiptTimeout > TimeSpan.Zero;
+        set => this.WaitForReceiptTimeout = value ? TimeSpan.FromMinutes(2) : TimeSpan.Zero;
+    }
+
 }
