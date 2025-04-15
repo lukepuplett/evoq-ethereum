72d6376f12521132e67ccda8cb8c69860902b941
Luke Puplett <lukepuplett@hotmail.com>
2025-04-15 17:14:09 +0100

    Added a new VerifyMessage overload which surfaces the error

--- src/Evoq.Ethereum/Ethereum.MessageSigning/MessageSigner.cs
+++ src/Evoq.Ethereum/Ethereum.MessageSigning/MessageSigner.cs
@@ -71,6 +71,19 @@ public class MessageSigner
     /// <returns>True if the signature is valid, false otherwise.</returns>
     public bool VerifyMessage(SigningPayload payload, IRsvSignature rsv, EthereumAddress expectedAddress)
     {
+        return VerifyMessage(payload, rsv, expectedAddress, out _);
+    }
+
+    /// <summary>
+    /// Verifies a message.
+    /// </summary>
+    /// <param name="payload">The payload to verify.</param>
+    /// <param name="rsv">The signature to verify.</param>
+    /// <param name="expectedAddress">The expected address.</param>
+    /// <param name="message">The message that was signed.</param>
+    /// <returns>True if the signature is valid, false otherwise.</returns>
+    public bool VerifyMessage(SigningPayload payload, IRsvSignature rsv, EthereumAddress expectedAddress, out string message)
+    {
         var recovery = new Secp256k1Recovery();
         try
         {
@@ -79,18 +92,30 @@ public class MessageSigner
 
             var recoveredAddress = EthereumAddress.FromPublicKey(new Hex(publicKey));
 
-            return recoveredAddress == expectedAddress;
+            if (recoveredAddress == expectedAddress)
+            {
+                message = "Signed by the expected address";
+                return true;
+            }
+            else
+            {
+                message = "Signed by a different address";
+                return false;
+            }
         }
-        catch (InvalidOperationException)
+        catch (InvalidOperationException invalidOp)
         {
+            message = "Invalid or malformed signature: " + invalidOp.Message;
             return false;
         }
-        catch (ArgumentException)
+        catch (ArgumentException badArg)
         {
+            message = "Invalid or malformed signature: " + badArg.Message;
             return false;
         }
         catch (NotImplementedException notImplemented) when (notImplemented.Message.Contains("Compressed public key support not yet implemented"))
         {
+            message = "Invalid or malformed signature: " + notImplemented.Message;
             return false;
         }
 
